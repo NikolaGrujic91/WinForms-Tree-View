a7f5cb3b9be8c3ff1941f639b07a747edec1e2b5
Nikola Grujić <grujic.nikola91@gmail.com>
2020-03-08 10:47:58 +0100

    Added project and updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# WinForms-Tree-View
+# WinForms Tree View
+
+This repository contains implementation of basic TreeView.
+
+[WinForms Examples](https://github.com/NikolaGrujic91/WinForms-Examples)
+
+![image missing](App.png "Application UI")
---
+++ WinFormsTreeView/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsTreeView
+{
+    public partial class Form1 : Form
+    {
+        #region Constructors
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Events
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var node = this.treeView.Nodes.Add("Fruits");
+            node.Nodes.Add("Apple");
+            node.Nodes.Add("Peach");
+
+            node = this.treeView.Nodes.Add("Vegetables");
+            node.Nodes.Add("Tomato");
+            node.Nodes.Add("Eggplant");
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Action == TreeViewAction.ByMouse)
+            {
+                MessageBox.Show(e.Node.FullPath);
+            }
+        }
+
+        #endregion
+    }
+}
